1d54a2c8365f85c9c72527e852f7c696c1875531
John Lai <lai.john.py@gmail.com>
2024-01-03 18:42:13 -0500

    Add custom js for testing

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,12 @@
-// Put your application javascript here
+document.addEventListener('DOMContentLoaded', function () {
+  const btn = document.getElementById('test-btn');
+
+  if (btn) {
+    btn.addEventListener('click', function () {
+      // Your custom code to be executed when the button is clicked
+      console.log('Button clicked!');
+
+      this.innerHTML = 'this works';
+    });
+  }
+});
