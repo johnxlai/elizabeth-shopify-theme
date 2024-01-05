90b99f883577b289070e9208a27f538219eb50cc
John Lai <lai.john.py@gmail.com>
2024-01-04 23:31:43 -0500

    Add new script and update js

--- assets/application.js
+++ assets/application.js
@@ -1,11 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const btns = document.querySelectorAll('.add-to-cart__btn');
-
-  btns.forEach((btn) => {
-    btn.addEventListener('mouseover', function () {
-      // Your custom code to be executed when the button is clicked
-      this.innerHTML = 'this works';
-      console.log(this.dataset.product);
-    });
-  });
+  // const btns = document.querySelectorAll('.add-to-cart__btn');
+  // btns.forEach((btn) => {
+  //   btn.addEventListener('mouseover', function () {
+  //     // Your custom code to be executed when the button is clicked
+  //     this.innerHTML = 'this works';
+  //     console.log(this.dataset.product);
+  //   });
+  // });
 });
