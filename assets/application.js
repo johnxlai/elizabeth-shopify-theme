3bbb7765026aac17a9c6a45632d776c34b7962c9
John Lai <lai.john.py@gmail.com>
2024-01-04 22:24:40 -0500

    Testing custom js

--- assets/application.js
+++ assets/application.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const btn = document.getElementById('test-btn');
+  const btns = document.querySelectorAll('.add-to-cart__btn');
 
-  if (btn) {
-    btn.addEventListener('click', function () {
+  btns.forEach((btn) => {
+    btn.addEventListener('mouseover', function () {
       // Your custom code to be executed when the button is clicked
-      console.log('Button clicked!');
       this.innerHTML = 'this works';
+      console.log(this.dataset.product);
     });
-  }
+  });
 });
